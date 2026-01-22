ea419b745ad5a025df9ad3a136ca01752c16db2e
Daniel Wagner <dnl.wagner@gmail.com>
2026-01-21 18:22:21 -0800

    init wifi - part 0

--- src/main/main.c
+++ src/main/main.c
@@ -1,4 +1,7 @@
 #include "esp_log.h"
+#include "esp_netif.h"
+#include "esp_event.h"
+#include "esp_wifi.h"
 #include "esp_camera.h"
 #include "nvs_flash.h"
 #include "nvs.h"
@@ -89,13 +92,49 @@ static bool init_camera(void) {
   return err == ESP_OK;
 }
 
+void wifi_init_sta(void)
+{
+  ESP_ERROR_CHECK(esp_netif_init());
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+  esp_netif_create_default_wifi_sta();
+
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+
+  wifi_config_t wifi_config = {
+      .sta = {
+          .ssid = "robi-12345",
+          .password = "Robi54321",
+      },
+  };
+
+  ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+  ESP_ERROR_CHECK(esp_wifi_start());
+}
+
+
 void app_main(void)
 {
+  vTaskDelay(pdMS_TO_TICKS(3000));
   ESP_LOGI(TAG, "RobotCameraServer is starting");
   ESP_LOGI(TAG, "Initializing NVS");
-  nvs_flash_init();
+  esp_err_t ret = nvs_flash_init();
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK(ret);
+
   ESP_LOGI(TAG, "NVS initialized");
 
+  ESP_LOGI(TAG, "Initializing network");
+  wifi_init_sta();
+
+  ESP_LOGI(TAG, "Network initialized");
+
+  ESP_LOGI(TAG, "Initializing camera");
   bool camera_ok = init_camera();
   if (!camera_ok) {
     ESP_LOGE(TAG, "Camera failed to initialize");
